e5000b3bd9ea47edca3f92ed04c8701b4e4c18cb
Adam Barton <adam.barton333@gmail.com>
2018-10-19 21:55:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :recipes
+  end
+
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to '/recipes/:id'
@@ -19,10 +24,6 @@ class ApplicationController < Sinatra::Base
     erb :'recipes/new'
   end
 
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :recipes
-  end
 
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
