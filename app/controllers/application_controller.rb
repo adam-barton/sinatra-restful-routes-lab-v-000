75ef8ad08b956ada96b46b7357fdcf6f415abe46
Adam Barton <adam.barton333@gmail.com>
2018-10-19 21:28:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
 
+    redirect to '/recipes/:id'
   end
 
 
